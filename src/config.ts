c477aa3eaf35ac8aa2ddc41a19f404e6edf4ca29
hallm74 <68484824+hallm74@users.noreply.github.com>
2024-12-14 13:18:13 -0600

    edited config

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
+  website: "https://ontheoffbeat.com/", // replace this with your deployed domain
+  author: "Shannon Hall",
+  profile: "https://ontheoffbeat.com/about/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "On The Offbeat",
   ogImage: "astropaper-og.jpg",
